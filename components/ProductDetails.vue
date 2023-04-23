7e5d1035462fc330b2d8c3e30622e339dffbef6c
sebrs3018 <sebastian.r.s3107@hotmail.com>
2023-04-23 13:29:32 +0200

    fixed price on product details

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -9,7 +9,7 @@ const { product } = defineProps<{ product: Product | null }>();
     </div>
     <div class="p-7">
       <h2 class="text-4xl my-7">{{ product?.title }}</h2>
-      <p class="text-xl my-7">Price - ${{ product?.title }}</p>
+      <p class="text-xl my-7">Price - ${{ product?.price }}</p>
       <h3 class="font-bod border-b-2 mb-4 pb-2">Product Description:</h3>
       <p class="mb-7">{{ product?.price }}</p>
     </div>
